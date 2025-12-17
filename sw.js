64f60262a333cda4ca20b3f4ca3efa94ed2defec
Hanan Abid <hani.hanan1100@gmail.com>
2025-12-17 05:34:09 +0500

    Add service worker for caching resources
    Implement a simple service worker to cache resources for the PWA.

---
+++ sw.js
@@ -0,0 +1,26 @@
+// Simple Service Worker for PWA
+const CACHE_NAME = 'subhan-tool-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './profile.jpg'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Return cache or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
